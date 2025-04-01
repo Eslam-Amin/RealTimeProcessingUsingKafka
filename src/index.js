37f02241e0524366b07643510a2f5cf0ad93f3a2
Eslam Amin <eslam.amin.610@gmail.com>
2025-04-01 07:33:05 +0200

    Created Index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/mongo_db");
+const startConsumer = require("./config/kafka/consumer");
+const userActivityLogsRoutes = require("./routes/userActivityLogs.routes");
+const fakeUserRoutes = require("./routes/fakeUser.routes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use("/api/user-activity-logs", userActivityLogsRoutes);
+app.use("/api/fake", fakeUserRoutes);
+
+// Start Kafka Consumer
+startConsumer()
+  .then(() => console.log("Kafka Consumer Started"))
+  .catch((err) => {
+    console.log("🚀 ~ Error starting Kafka Consumer:", err);
+  });
+// Start Server & DB Connection
+connectDB().then(() => {
+  app.listen(3000, () => console.log(`Server running on port ${3000}`));
+});
