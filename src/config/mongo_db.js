b20e2aa713c822a728b1231120ed4ed57188095c
Eslam Amin <eslam.amin.610@gmail.com>
2025-04-03 00:51:37 +0200

    Updated Docker Compose to run mongodb on  the mongodb of the docker, saves the data to volumes

--- src/config/mongo_db.js
+++ src/config/mongo_db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const { MONGO_URI } = require("../env");
+const { MONGO_URI, MONGO_URI_LOCAL } = require("../env");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
+    await mongoose.connect(MONGO_URI_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
-    console.log("🚀 ~ connectDB ~ MONGO_URI:", MONGO_URI);
+    console.log("🚀 ~ connectDB ~ MONGO_URI:", MONGO_URI_LOCAL);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Connection Error", err);
